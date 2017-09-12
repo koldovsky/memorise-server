f0b070b384eadfb603da58c2695e6a6a3454e5f3
Nazar <nazariy84@gmail.com>
2017-09-12 18:03:22 +0300

    some changes

--- MemoRise/MemoRise/Controllers/CatalogController.cs
+++ MemoRise/MemoRise/Controllers/CatalogController.cs
@@ -11,7 +11,7 @@ using System.Web.Http.Cors;
 
 namespace MemoRise.Controllers
 {
-    //[EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
     public class CatalogController : ApiController
     {
 
