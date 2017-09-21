bb2c190a189920bb64328d0b55284b6a0de954af
NastyaKolomoets <nastya.kolomoets@gmail.com>
2017-09-21 20:39:11 +0300

    Add unit tests for Filter BLL

--- Memorise.Tests/FilterTests.cs
+++ Memorise.Tests/FilterTests.cs
@@ -1,30 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using MemoBll;
 using MemoDAL;
-using MemoDAL.Repositories.Interfaces;
+using MemoDAL.Entities;
 using Moq;
 using NUnit.Framework;
 
 namespace Memorise.Tests
 {
     [TestFixture]
+    //[TestFixtureSource("GetMockedSut")]
     class FilterTests
     {
-        private Filter GetMockedSut()
+        [Test]
+        public void GetAllCategoriesTest()
         {
-            Mock<ICategoryRepository> categories = new Mock<ICategoryRepository>();
-            Mock<ICourseRepository> courses = new Mock<ICourseRepository>();
+            // Arrange
+            var expected = new List<Category>();
+            for (int i = 0; i < 3; i++)
+            {
+                expected.Add(new Category { Id = i, Name = $"Category{i}" });
+            }
             Mock<IUnitOfWork> unitOfWork = new Mock<IUnitOfWork>();
-
-            unitOfWork.Setup(uow => uow.Categories).Returns(categories.Object);
-            unitOfWork.Setup(uow => uow.Courses).Returns(courses.Object);
-
+            unitOfWork.Setup(uow => uow.Categories.GetAll()).Returns(expected);
             var filter = new Filter(unitOfWork.Object);
-            return filter;
+
+            // Act
+            var actual = filter.GetAllCategories();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+            unitOfWork.Verify();
+        }
+
+        [Test]
+        public void GetAllCoursesTest()
+        {
+            // Arrange
+            var expected = new List<Course>();
+            for (int i = 0; i < 3; i++)
+            {
+                expected.Add(new Course { Id = i, Name = $"Course{i}" });
+            }
+            Mock<IUnitOfWork> unitOfWork = new Mock<IUnitOfWork>();
+            unitOfWork.Setup(uow => uow.Courses.GetAll()).Returns(expected);
+            var filter = new Filter(unitOfWork.Object);
+
+            // Act
+            var actual = filter.GetAllCourses();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+            unitOfWork.Verify();
+        }
+
+        [Test]
+        public void GetCategoryTest()
+        {
+            // Arrange
+            int id = 1;
+            var expected = new Category { Id = id, Name = $"Category1" };
+            
+            Mock<IUnitOfWork> unitOfWork = new Mock<IUnitOfWork>();
+            unitOfWork.Setup(uow => uow.Categories.Get(id)).Returns(expected);
+            var filter = new Filter(unitOfWork.Object);
+
+            // Act
+            var actual = filter.GetCategory(id);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+            unitOfWork.Verify();
         }
     }
 }
