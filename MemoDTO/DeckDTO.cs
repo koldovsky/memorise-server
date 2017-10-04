8d8c91db2c415e2ebf6feb7b15f65f11c5ce095d
YaroslavKormushyn <kormushyn.slavik@gmail.com>
2017-10-05 00:34:52 +0300

    Added remote image url injection with Flickr.Net.

--- MemoDTO/DeckDTO.cs
+++ MemoDTO/DeckDTO.cs
@@ -5,6 +5,7 @@
         public string Name { get; set; }
         public int Price { get; set; }
         public int CardsNumber { get; set; }
+		public string Photo { get; set; }
         public string Linking { get; set; }
 	}
 }
