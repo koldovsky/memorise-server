8f21c54f0188bb7cfd39515a4a00079c6efaceb8
SerhiiZinkiv <agrail1307@gmail.com>
2017-10-14 11:32:09 +0300

    add ByPage Controller

--- MemoRise/Controllers/CatalogController.cs
+++ MemoRise/Controllers/CatalogController.cs
@@ -8,12 +8,14 @@ using System.Linq;
 using MemoRise.Helpers;
 using System.Configuration;
 using FlickrNet;
+using PagedList;
+using PagedList.Mvc;
 
 namespace MemoRise.Controllers
 {
-	public class CatalogController : ApiController
-	{
-		CatalogBll catalog = new CatalogBll();
+    public class CatalogController : ApiController
+    {
+        CatalogBll catalog = new CatalogBll();
 
         [HttpGet]
         public IHttpActionResult GetCategories()
@@ -22,12 +24,12 @@ namespace MemoRise.Controllers
             {
                 List<CategoryDTO> categories = catalog.GetAllCategories()
                                                .ToList();
-                return  Ok(categories);
+                return Ok(categories);
             }
             catch (ArgumentNullException ex)
             {
                 var message = $"Categories collection is empty.";
-                return  BadRequest(ex.Message);
+                return BadRequest(ex.Message);
             }
             catch (Exception ex)
             {
@@ -35,151 +37,230 @@ namespace MemoRise.Controllers
             }
         }
 
-		[HttpGet]
-		public IHttpActionResult GetCourses()
-		{
-			try
-			{
-				List<CourseDTO> courses = catalog.GetAllCourses().ToList();
-				PhotoUrlLoader.LoadCoursesPhotos(courses);
-				return Ok(courses);
-			}
-			catch (ArgumentNullException ex)
-			{
-				var message = $"Courses collection is empty. {ex.Message}";
-				return BadRequest(message);
-			}
-			catch (Exception ex)
-			{
-				return BadRequest(ex.Message);
-			}
-		}
+        [HttpGet]
+        public IHttpActionResult GetCourses()
+        {
+            try
+            {
+                List<CourseDTO> courses = catalog.GetAllCourses().ToList();
+                PhotoUrlLoader.LoadCoursesPhotos(courses);
+                return Ok(courses);
+            }
+            catch (ArgumentNullException ex)
+            {
+                var message = $"Courses collection is empty. {ex.Message}";
+                return BadRequest(message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
 
-		[HttpGet]
-		//[Authorize(Roles = "Customer")]
-		public IHttpActionResult GetDecks()
-		{
-			try
-			{
-				List<DeckDTO> decks = catalog.GetAllDecks().ToList();
-				PhotoUrlLoader.LoadDecksPhotos(decks);
+        [HttpGet]
+        [Route("Catalog/GetCoursesByPage/{page}/{pageSize}")]
+        public IHttpActionResult GetCoursesByPage(int page, int pageSize)
+        {
+            try
+            {
+                List<CourseDTO> courses;
+                if (page == 0 && pageSize == 0)
+                {
+                    courses = catalog.GetAllCourses().ToList();
+                }
+                else
+                {
+                    courses = catalog.GetAllCourses().Skip((page - 1) * pageSize).Take(pageSize).ToList();
+                }
+                var temp = new { items = courses, totalCount = courses.Count };
+                return Ok(temp);
+            }
+            catch (ArgumentNullException ex)
+            {
+                var message = $"Courses collection is empty. {ex.Message}";
+                return BadRequest(message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
 
-				return Ok(decks);
-			}
-			catch (ArgumentNullException ex)
-			{
-				var message = $"Decks collection is empty. {ex.Message}";
-				return BadRequest(message);
-			}
-			catch (Exception ex)
-			{
-				return BadRequest(ex.Message);
-			}
-		}
+        [HttpGet]
+        //[Authorize(Roles = "Customer")]
+        public IHttpActionResult GetDecks()
+        {
+            try
+            {
+                List<DeckDTO> decks = catalog.GetAllDecks().ToList();
+                PhotoUrlLoader.LoadDecksPhotos(decks);
 
-		[HttpGet]
-		[Route("Catalog/GetCoursesByCategory/{categoryName}")]
-		public IHttpActionResult GetCoursesByCategory(string categoryName)
-		{
-			try
-			{
-				IEnumerable<CourseDTO> courses = catalog.
-										  GetAllCoursesByCategory(categoryName);
-				if (courses == null)
-				{
-					throw new Exception("Courses aren't found by this category!");
-				}
-				PhotoUrlLoader.LoadCoursesPhotos(courses);
+                return Ok(decks);
+            }
+            catch (ArgumentNullException ex)
+            {
+                var message = $"Decks collection is empty. {ex.Message}";
+                return BadRequest(message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
 
-				return Ok(courses.ToList());
-			}
-			catch (ArgumentNullException ex)
-			{
-				var message = $"Category with name = {categoryName} " +
-							  $"not found. {ex.Message}";
-				return BadRequest(message);
-			}
-			catch (Exception ex)
-			{
-				return BadRequest(ex.Message); ;
-			}
-		}
+        [HttpGet]
+        [Route("Catalog/GetDecksByPage/{page}/{pageSize}")]
+        public IHttpActionResult GetDecksByPage(int page, int pageSize)
+        {
+            try
+            {
+                List<DeckDTO> decks;
+                if (page == 0 && pageSize == 0)
+                {
+                    decks = catalog.GetAllDecks().ToList();
+                }
+                else
+                {
+                    decks = catalog.GetAllDecks().Skip((page - 1) * pageSize).Take(pageSize).ToList();
+                }
+                var temp = new { items = decks, totalCount = decks.Count };
+                return Ok(temp);
+            }
+            catch (ArgumentNullException ex)
+            {
+                var message = $"Courses collection is empty. {ex.Message}";
+                return BadRequest(message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
 
-		[HttpGet]
-		[Route("Catalog/GetDecksByCategory/{categoryName}")]
-		public IHttpActionResult GetDecksByCategory(string categoryName)
-		{
-			try
-			{
-				IEnumerable<DeckDTO> decks = catalog
-									 .GetAllDecksByCategory(categoryName);
-				if (decks == null)
-				{
-					throw new Exception("Decks aren't found by this category!");
-				}
-				PhotoUrlLoader.LoadDecksPhotos(decks);
+        //[HttpGet]
 
-				return Ok(decks.ToList());
-			}
-			catch (ArgumentNullException ex)
-			{
-				var message = $"Category with name = {categoryName} " +
-							  $"not found. {ex.Message}";
-				return BadRequest(message);
-			}
-			catch (Exception ex)
-			{
-				return BadRequest(ex.Message);
-			}
+        //public IHttpActionResult GetSortedDecks()
+        //{
+        //    try
+        //    {
+        //        IEnumerable<DeckDTO> decks = catalog.GetAllDecks().OrderBy(s => s.Name);
 
-		}
+        //        return Ok();
+        //    }
+        //    catch (ArgumentNullException ex)
+        //    {
+        //        var message = $"Decks collection is empty. {ex.Message}";
+        //        return BadRequest(message);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
 
-		[HttpGet]
-		[Route("Catalog/GetAllDecksByCourse/{courseName}")]
-		public IHttpActionResult GetAllDecksByCourse(string courseName)
-		{
-			try
-			{
-				List<DeckDTO> decks = catalog.GetAllDecksByCourse(courseName)
-									 .ToList();
-				PhotoUrlLoader.LoadDecksPhotos(decks);
-				return Ok(decks);
-			}
-			catch (ArgumentNullException ex)
-			{
-				var message = $"Course with name = {courseName} " +
-							  $"not found. {ex.Message}";
-				return BadRequest(message);
-			}
-			catch (Exception ex)
-			{
-				return BadRequest(ex.Message);
-			}
-		}
+        [HttpGet]
+        [Route("Catalog/GetCoursesByCategory/{categoryName}")]
+        public IHttpActionResult GetCoursesByCategory(string categoryName)
+        {
+            try
+            {
+                IEnumerable<CourseDTO> courses = catalog.
+                                          GetAllCoursesByCategory(categoryName);
+                if (courses == null)
+                {
+                    throw new Exception("Courses aren't found by this category!");
+                }
+                PhotoUrlLoader.LoadCoursesPhotos(courses);
 
-		[HttpGet]
-		[Route("Catalog/GetCourse/{courseName}")]
-		public IHttpActionResult GetCourse(string courseName)
-		{
-			try
-			{
+                return Ok(courses.ToList());
+            }
+            catch (ArgumentNullException ex)
+            {
+                var message = $"Category with name = {categoryName} " +
+                              $"not found. {ex.Message}";
+                return BadRequest(message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message); ;
+            }
+        }
 
-				CourseWithDecksDTO course = catalog
-										   .GetCourseWithDecksDTO(courseName);
-				PhotoUrlLoader.LoadCourseAndDecksPhotos(course);
+        [HttpGet]
+        [Route("Catalog/GetDecksByCategory/{categoryName}")]
+        public IHttpActionResult GetDecksByCategory(string categoryName)
+        {
+            try
+            {
+                IEnumerable<DeckDTO> decks = catalog
+                                     .GetAllDecksByCategory(categoryName);
+                if (decks == null)
+                {
+                    throw new Exception("Decks aren't found by this category!");
+                }
+                PhotoUrlLoader.LoadDecksPhotos(decks);
 
-				return Ok(course);
-			}
-			catch (ArgumentNullException ex)
-			{
-				var message = $"Course with name = {courseName} " +
-							  $"not found. {ex.Message}";
-				return BadRequest(message);
-			}
-			catch (Exception ex)
-			{
-				return BadRequest(ex.Message);
-			}
-		}
-	}
+                return Ok(decks.ToList());
+            }
+            catch (ArgumentNullException ex)
+            {
+                var message = $"Category with name = {categoryName} " +
+                              $"not found. {ex.Message}";
+                return BadRequest(message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpGet]
+        [Route("Catalog/GetAllDecksByCourse/{courseName}")]
+        public IHttpActionResult GetAllDecksByCourse(string courseName)
+        {
+            try
+            {
+                List<DeckDTO> decks = catalog.GetAllDecksByCourse(courseName)
+                                     .ToList();
+                PhotoUrlLoader.LoadDecksPhotos(decks);
+                return Ok(decks);
+            }
+            catch (ArgumentNullException ex)
+            {
+                var message = $"Course with name = {courseName} " +
+                              $"not found. {ex.Message}";
+                return BadRequest(message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("Catalog/GetCourse/{courseName}")]
+        public IHttpActionResult GetCourse(string courseName)
+        {
+            try
+            {
+
+                CourseWithDecksDTO course = catalog
+                                           .GetCourseWithDecksDTO(courseName);
+                PhotoUrlLoader.LoadCourseAndDecksPhotos(course);
+
+                return Ok(course);
+            }
+            catch (ArgumentNullException ex)
+            {
+                var message = $"Course with name = {courseName} " +
+                              $"not found. {ex.Message}";
+                return BadRequest(message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
 }
