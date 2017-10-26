f21082715cd62b1d87b955f0e944ba347cf330cd
SerhiiZinkiv <agrail1307@gmail.com>
2017-10-26 15:06:43 +0300

    fix bug

--- MemoRise/Models/SearchDataModel.cs
+++ MemoRise/Models/SearchDataModel.cs
@@ -7,7 +7,6 @@ namespace MemoRise.Models
 {
     public class SearchDataModel
     {
-        //int page, int pageSize, bool sort, string searchString
         public int Page { get; set; }
         public int PageSize { get; set; }
         public bool Sort { get; set; }
